94ea031a7963320fd4c8761e50e114f96cdee727
kingame <truongthinh619@gmail.com>
2026-06-08 22:14:05 +0700

    Fix lỗi hiển thị icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1021790963" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1573665765" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
